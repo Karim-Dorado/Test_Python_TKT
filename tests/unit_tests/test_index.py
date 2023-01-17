5951da7927980ff549e610b32a1d8d4b059feeb7
Karim-Dorado <karimdorado@gmail.com>
2023-01-17 20:20:40 +0100

    update test_first_table_raw

--- tests/unit_tests/test_index.py
+++ tests/unit_tests/test_index.py
@@ -5,7 +5,7 @@ def test_should_status_code_ok(client):
 
 def test_first_table_raw(client):
     response = client.get('/')
-    assert ("Name") in response.data.decode()
+    assert ("Company") in response.data.decode()
     assert ("Sector") in response.data.decode()
     assert ("Siren") in response.data.decode()
 
